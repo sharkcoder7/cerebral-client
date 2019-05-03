d25e3c0345ccc62f6e90a476c68a491192b01217
Peter O'Leary <pete@timelight.com>
2019-05-02 17:48:01 -0700

    Proxy tweak for Heroku servers

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,5 +3,5 @@ const proxy = require('http-proxy-middleware');
 require('dotenv').config();
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: process.env.API_SERVER_URL }));
+  app.use(proxy('/api', { target: process.env.API_SERVER_URL, changeOrigin: true }));
 };
