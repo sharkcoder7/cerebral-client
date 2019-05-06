65c8b9133485122bd81428ad8eab218ae729b73d
taejun song <taejun.song@taejuns-MacBook-Pro.local>
2019-05-06 14:32:28 -0700

    Add file uploader components

--- src/components/video_recorder/index.js
+++ src/components/video_recorder/index.js
@@ -9,22 +9,38 @@ class VideoRecorderComponent extends Component {
 
 	componentDidMount(){
 		console.log("com mount: ", this.refs.recorder)
+		
 	}
 
 	componentDidUpdate(){
 		console.log("com update: ", this.refs.recorder)
 	}
-	record_handler = () => {
-				const screenshot = this.refs.recorder
-				console.log(this.refs.recorder.mediaRecorder)
-				//this.setState({screenshot})
+
+	//TODO: make actio for submit video, call api and update into s3
+	submit_handler = () => {
+		const  data= this.refs.recorder
+		const {submit_video} = this.props
+		console.log(this.refs.recorder.mediaRecorder)
+		//submit_video({data})
 	}
 
   render() {
     return (
 			<div>
-				<VideoRecorder ref='recorder'/>
-				<input type="button" value="capture image" onClick={this.record_handler}/>
+				<div className = "d-flex justify-content-center text-big">
+					<p>Tell me us more about why you're seeking out a prescription for antidepressants</p>
+				</div>	
+				<div className = "d-flex justify-content-center text-small">
+					<p>(approximately 2 minutes)</p>
+				</div>
+				<div className = "video_cam_holder">
+					<VideoRecorder ref='recorder' timeLimit='120000'/>
+				</div>
+				<div className = "d-flex justify-content-center p-2">	
+					<input className="col btn-confirm text-btn" type="button" value="Submit Video" 
+						onClick={this.submit_hanlder}/>	
+				</div>
+		
 			</div>
 		)
   }
