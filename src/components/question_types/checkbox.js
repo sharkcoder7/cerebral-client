155dcc3c4146b3217a2b1d1f3d38e06975ba0754
deneb0618 <techdeneb@gmail.com>
2019-05-08 09:47:29 -0700

    basic events for checkbox component

--- src/components/question_types/checkbox.js
+++ src/components/question_types/checkbox.js
@@ -1,40 +1,48 @@
 import React, {Component} from 'react'
 import uuidv1 from  'uuid/v1';
 
-class CheckBoxComponents extends Component {
+class CheckBoxComponent extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			selected:''
+			isselected:'',
+			selected_item:''
 		}
 	}
 
-	check_box_handler = e => {
+	check_box_handler = (e,item) => {
 		//update local state in here
+		this.setState({selected_item:item})
 	}
 
 	submit_handler = () => {
 		//call action from parents with this.state.selected 
-
+		if(selected_item){
+		
+		}
 	}
-
+	
 	map_data_to_checkbox = item => {
-		<div className="input-group mb-3" key={uuidv1()}>
-			<div className="input-group-prepend">
-				<div className="input-group-text group-checkbox">
-					<input type="checkbox" onChange={this.check_box_handler}>
+		return(
+			<div className="input-group mb-3" key={uuidv1()}>
+				<div className="input-group-prepend">
+					<div className="input-group-text group-checkbox">
+						<input type="checkbox" onClick={(e) => 
+								this.check_box_handler(e,item)}/>
+					</div>
+				</div>
+				<div className="d-flex justify-content-center form-control group-checkbox-text">
+					<p className="text-small">{item}</p>
 				</div>
 			</div>
-			<div className="d-flex justify-content-center form-control group-checkbox-text">
-				<p className="text-small">{item}</p>
-			</div>
-		</div>
-}
+			)
+		}
 
 	render(){
-		return this.items.options.map((item, index) => (
-			map_item_to_checkbox(item)
+		return this.item.map((item, index) => (
+			this.map_data_to_checkbox(item)
 		))
 	}
 }
 
+export default CheckBoxComponent
