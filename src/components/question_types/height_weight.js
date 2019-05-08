10956dfd7742b4d7f003fc5963f5a7880c595ef3
taejun song <taejun.song@taejuns-MacBook-Pro.local>
2019-05-07 17:53:01 -0700

    bug fix for height weight component

--- src/components/question_types/height_weight.js
+++ src/components/question_types/height_weight.js
@@ -10,8 +10,8 @@ export const height_weight = (event_handler) => {
 					 <div className ="input-group-prepend text_field_height_weight">
 						 <span className = "input-group-text span_height_weight">Height</span>
 					 </div>
-					 <input type="text" placeholder="Ft" className="form-control input_height_weight">
-					 <input type="text" placeholder="In" className="form-control input_height_weight">
+					 <input type="text" placeholder="Ft" className="form-control input_height_weight"/>
+					 <input type="text" placeholder="In" className="form-control input_height_weight"/>
 				 </div>
 			 </div>
 			{components.confirm_button_type_1(event_handler, "Confirm weight and height")}
