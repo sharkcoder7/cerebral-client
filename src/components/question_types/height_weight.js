39124d7535147976fa91f2f0f4fab2250fded295
taejun song <taejun.song@taejuns-MacBook-Pro.local>
2019-05-07 17:14:17 -0700

    height_weight component style

--- src/components/question_types/height_weight.js
+++ src/components/question_types/height_weight.js
@@ -5,13 +5,15 @@ export const height_weight = (event_handler) => {
 	return (
 		<div>
 			{components.input_type_1(null, "Weight lbs")}
-			<div className = "input-group input-type1 height_weight">
-				<div className="input-group-prepend">
-    			<span className="input-group-text">Height</span>
-  			</div>
-				<input type="text" placeholder="Ft" className="form-control" />
-				<input type="text" placeholder="In" className="form-control"/>
-			</div>
+			<div className = "p-2">
+				 <div className = "input-group input-type1">
+					 <div className ="input-group-prepend text_field_height_weight">
+						 <span className = "input-group-text span_height_weight">Height</span>
+					 </div>
+					 <input type="text" placeholder="Ft" className="form-control input_height_weight">
+					 <input type="text" placeholder="In" className="form-control input_height_weight">
+				 </div>
+			 </div>
 			{components.confirm_button_type_1(event_handler, "Confirm weight and height")}
 
 		</div>
