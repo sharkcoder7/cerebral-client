a369f954d1e8c39bc3738309d142c444b4f48a4b
deneb0618 <techdeneb@gmail.com>
2019-05-14 17:07:56 -0700

    Add validation checking for checkbox, height_weight and text area

--- src/components/question_types/checkbox.js
+++ src/components/question_types/checkbox.js
@@ -6,29 +6,48 @@ class CheckBoxComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isselected:'',
-      selected_items:{}
+      items:[],
+      msg:''
     }
   }
 
+  componentDidMount(){
+    //initalize item
+    if(this.props.items && this.props.items.options){
+      const arr = new Array(this.props.items.options.length).fill(false);     
+      this.setState({items:arr})
+    }
+  }
+
+
   //TODO: Save multiple values from selected boxes
-  check_box_handler = (e,item) => {
-    //update local state in here
-    //this.setState({selected_item:item})
+  check_box_handler = (e, idx) => {
+    var list = this.state.items
+    list[idx]=e.target.checked
+    this.setState({items:list})
   }
 
   submit_btn_handler = () => {
     //call action from parents with this.state.selected 
-    this.props.submit_action()
+    var info=''
+    this.state.items.forEach((val, idx)=>{
+      if(val) info+=this.props.items.options[idx].option_name+" ,"
+    }) 
+    console.log("rst: ", info.slice(0,-1)) 
+    if(info){ 
+      this.props.submit_action(info.slice(0, -1))
+    }else{
+      this.setState({msg:"Please select at least one item"})
+    }
   }
   
   //{(e) => this.check_box_handler(e,item.option_name)}
-  map_data_to_checkbox = item => {
+  map_data_to_checkbox = (item,index) => {
     return(
       <div className="input-group mb-3" key={uuidv1()}>
         <div className="input-group-prepend">
           <div className="input-group-text group-checkbox">
-            <input type="checkbox" name={item.option_name}/>
+            <input type="checkbox" onChange={(e) => {this.check_box_handler(e,index)}} name={item.option_name} checked={this.state.items[index]}/>
           </div>
         </div>
         <div className="d-flex justify-content-center form-control group-checkbox-text">
@@ -41,7 +60,9 @@ class CheckBoxComponent extends Component {
   render(){
     return (
       <div>    
-        {this.props.items.options.map((item, index) => (this.map_data_to_checkbox(item)))}
+
+        <div className = "d-flex justify-content-center p-2 text-small-red">{this.state.msg}</div>
+        {this.props.items.options.map((item, index) => (this.map_data_to_checkbox(item, index)))}
         <div className="d-flex flex-row justify-content-center">
         {components.confirm_button_type_1(this.submit_btn_handler, "Confirm")}  
         </div>
--- src/components/question_types/text_area.js
+++ src/components/question_types/text_area.js
@@ -2,31 +2,37 @@ import React, {Component} from 'react'
 import * as components from '../question_components/components'
 
 class TextArea extends Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			text:''
-		}
-	}
+  constructor(props){
+    super(props)
+    this.state = {
+      text:'',
+      msg:''
+    }
+  }
 
   update_text_handler = e => { 
     this.setState({text:e.target.value})
   }
 
   submit_btn_handler = e => {
-    this.props.submit_action(this.state.text) 
+    if(this.state.text){ 
+      this.props.submit_action(this.state.text) 
+    }else{
+      this.setState({msg:"Please answer the question. If not applicable, please input N/A"})
+    }
   }
 
-	render(){	
-		return(
-			<div>
+  render(){ 
+    return(
+      <div>
+        <div className = "d-flex justify-content-center p-2 text-small-red">{this.state.msg}</div>
         <div className="form-group">
           <textarea onChange={this.update_text_handler} className="form-control" rows="5" />
         </div>
-        {components.confirm_button_type_1(this.submit_btn_handler, "Confirm your answer")}	
-			</div>
-		)
-	}
+        {components.confirm_button_type_1(this.submit_btn_handler, "Confirm your answer")}  
+      </div>
+    )
+  }
 }
 
 export default TextArea
