6bd9ce4474ba34db8bd8f05e9e9b7b8c155fb12a
Peter O'Leary <pete@timelight.com>
2019-05-21 14:51:21 -0700

    Add Stripe to client with basic ugly CC form

--- src/components/question_types/patient_payment.js
+++ src/components/question_types/patient_payment.js
@@ -2,42 +2,33 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import * as components from '../question_components/components'
 import { create_payment } from '../../actions/patient_action'
+import {CardElement, Elements, StripeProvider} from 'react-stripe-elements'
+import PaymentElements from './payment_elements'
 
 class PatientPayment extends Component {
 
   constructor(props){
     super(props)
-    this.state = {
-      payment_full_name: '',
-      payment_card_number:'',
-      payment_exp_month:'',
-      payment_exp_year:'',
-      payment_cc:''
-    }
   }
 
-  update_handler = (e) => { 
+  componentDidMount = () => {
+  }
 
-    create_payment(this.state.payment_full_name, this.state.payment_card_number, this.state.payment_exp_month, this.state.payment_exp_year, this.state.payment_cc).then((resp) => {
-      
+  submit_payment = (payment_full_name, token) => {
+    this.props.create_payment(payment_full_name, token).then((resp) => {
+        
       // DO NOT SEND PAYMENT INFORMATION to submit_action here IT WILL END UP IN THE DATABASE
-      this.props.submit_action(resp.transaction_code) 
-    })
-   }
-
-   update_property = (which, e) => {
-       this.state[which] = e.target.value
-   }
+      return this.props.submit_action(resp.transaction_code) 
+  })
+  }
   
   render(){
     return (
-      <div className="patient_shipping">
-        {components.input_type_autocomplete(this.update_property.bind(this, 'payment_full_name'), "Card Holder Name", 'cc-name')}
-        {components.input_type_autocomplete(this.update_property.bind(this, 'payment_card_number'), "Card Number", 'cc-number')}
-        {components.input_type_autocomplete(this.update_property.bind(this, 'payment_exp_year'), "Expiration MM-YYYY", 'cc-exp')}
-        {components.input_type_autocomplete(this.update_property.bind(this, 'payment_cvc'), "CC", 'cc-csc')}
-        {components.confirm_button_type_1(this.update_handler.bind(this), "Confirm Payment Information")}
-      </div>
+      <StripeProvider apiKey="pk_test_UARvRgKLIpQAbSeJrjRdd45K003KBuhOEM">
+        <Elements>
+      <PaymentElements submit_payment = {this.submit_payment.bind(this)} />
+      </Elements>
+      </StripeProvider>
     );
   }
 }
