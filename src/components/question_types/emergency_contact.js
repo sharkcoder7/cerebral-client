7f9e8a6dadaa9531d0a88b285bbec3d4969869ee
deneb0618 <techdeneb@gmail.com>
2019-06-11 10:11:57 -0700

    bug fix by remove checkbox

--- src/components/question_types/emergency_contact.js
+++ src/components/question_types/emergency_contact.js
@@ -9,20 +9,17 @@ class EmergencyContact extends Component {
       phone:'',
       first_name:'',
       last_name:'',
-      is_consent:false,
     }
   }
 
   update_handler = e => { 
-    const {is_consent, first_name, last_name, phone} = this.state
-    if(is_consent && phone && first_name && last_name){
+    const {first_name, last_name, phone} = this.state
+    if(phone && first_name && last_name){
       let ans = "first_name: "+first_name+" last_name: "+last_name+" phone: "+phone
       this.props.submit_action(ans) 
     }else{
       if(!phone || !first_name || !last_name){ 
         this.setState({msg:"Please fill all the fields"}) 
-      }else if(!is_consent){
-        this.setState({msg:"Please agree to the terms and conditions"})
       }    
     }
    }
@@ -42,10 +39,6 @@ class EmergencyContact extends Component {
     this.setState({last_name:lname})
   }
   
-  set_concensus = e => {
-    this.setState({is_consent:!this.state.is_consent})
-  }
-
   render(){
     let btn_wording = 'Confirm contact information >'
     return (
@@ -56,9 +49,6 @@ class EmergencyContact extends Component {
           {components.input_type_half(this.update_lastname.bind(this), "Last Name")}
         </div>
         {components.input_type_1(this.update_phone.bind(this), "Phone #")}
-        <div className="d-flex flex-row justify-content-start">
-        {components.checkbox_type_1(this.set_concensus.bind(this), 'I’ve read the No harm agreement and have made the commitment to not harm myself or anyone else in any way. I will not attempt suicide, or any other self injury. ')}
-        </div>
         {components.confirm_button_type_1(this.update_handler.bind(this), btn_wording)}
       </div>
     );
