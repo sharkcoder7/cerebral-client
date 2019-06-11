3b6707dd0751ac0b59d200455a16f047cd0d3305
deneb0618 <techdeneb@gmail.com>
2019-06-11 14:56:00 -0700

    bug fix

--- src/utils/common.js
+++ src/utils/common.js
@@ -37,11 +37,11 @@ export const map_type_to_component = (question, handlers, user, subscript_ref) =
   if(!question) return null
 
   switch(question.question_type) {
-    case 'emergency_contact':
+    case 'identification_cover':
       return <IdentificationCover submit_action = {handlers.next_step_handler}/>
     case 'selector':
       return selector(handlers.set_selector_handler, question)
-    case 'emergency_contactq':
+    case 'emergency_contact':
       return <EmergencyContact submit_action = {handlers.submit_answer_and_next_step}/>
     case 'create_profile':
       return <RegisterManager
