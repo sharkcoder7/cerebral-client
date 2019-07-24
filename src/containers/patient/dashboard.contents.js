acdfbb4f975f616b0092056a69980e4ec758d2ce
Anmol Arora <anmola@thoughtworks.com>
2019-07-24 23:58:11 +0530

    [Anmol] adds mobile view for patient dashboard and restructures desktop view to match designs, introduces eslint

--- src/containers/patient/dashboard.contents.js
+++ src/containers/patient/dashboard.contents.js
@@ -1,83 +1,94 @@
-import React, {Component} from 'react';
-import { withRouter } from "react-router-dom"
-import {connect} from 'react-redux'
-import EditProfile from '../../components/dashboard/edit_profile'
-import EditPassword from '../../components/dashboard/edit_password'
-import ShippingInformation from '../../components/dashboard/shipping_information'
-import PaymentInformation from '../../components/dashboard/payment_information'
-import MessageProcessManager from '../../components/dashboard/message_process_manager'
-import SubscriptionInformation from '../../components/dashboard/subscription_information'
-import Assessment from '../../components/dashboard/assessment'
-import {get_patient_shipping_address} from '../../actions/patient_action'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import EditProfile from '../../components/dashboard/edit_profile';
+import EditPassword from '../../components/dashboard/edit_password';
+import ShippingInformation from '../../components/dashboard/shipping_information';
+import PaymentInformation from '../../components/dashboard/payment_information';
+import MessageProcessManager from '../../components/dashboard/message_process_manager';
+import SubscriptionInformation from '../../components/dashboard/subscription_information';
+import Assessment from '../../components/dashboard/assessment';
+import { get_patient_shipping_address } from '../../actions/patient_action';
+import './dashboard.contents.scss';
 
-
-//TODO: will use it as wrapper 
-class DashboardContents extends Component{
-
-  constructor(props){
-    super(props)
+// TODO: will use it as wrapper
+class DashboardContents extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      user:this.props.user,
-      type:this.props.type,
-      patients_list:this.props.patients_list,
-      shipping_address: {address_1: null, address_2: null, city: null, region: null, postal_code: null},
-
-    }
+      user: this.props.user,
+      type: this.props.type,
+      patients_list: this.props.patients_list,
+      shipping_address: {
+        address_1: null,
+        address_2: null,
+        city: null,
+        region: null,
+        postal_code: null
+      }
+    };
   }
 
   componentDidMount = () => {
-    this.props.get_patient_shipping_address().then((data) => {
-      this.setState({shipping_address: data[0]})
-    })
-  }  
+    this.props.get_patient_shipping_address().then(data => {
+      this.setState({ shipping_address: data[0] });
+    });
+  };
 
-
-  componentWillReceiveProps = (next_props) => { 
+  componentWillReceiveProps = next_props => {
     window.scrollTo(0, 0);
-    this.setState({user:next_props.user, type:next_props.type, patient_list:next_props.patient_list}) 
-  }
+    this.setState({
+      user: next_props.user,
+      type: next_props.type,
+      patient_list: next_props.patient_list
+    });
+  };
 
-
-  //move xxx_view into common area and pass as parameter
-  patient_info_view = () =>(
-      <div className="d-flex flex-column profile-main-content">
-        <div className="d-flex justify-content-end text-main-title">Profile Information</div>
-        <div className="d-flex flex-column main-content-column">
-          <div className="d-flex flex-row justify-content-between main-content-row flex-wrap">
-            <EditProfile attr={this.state.user.attributes}/>
-            <ShippingInformation attr={this.state.shipping_address}/>
-          </div>
-          <div className="d-flex flex-row justify-content-between main-content-row flex-wrap">
-            <EditPassword attr={this.state.user.attributes}/>
-            <PaymentInformation patient={this.state.user.attributes.patient}/>
-          </div>
-        
+  // move xxx_view into common area and pass as parameter
+  patient_info_view = () => (
+    <div className="d-flex flex-column profile-main-content">
+      <div className="d-flex justify-content-end text-main-title desktop-only">
+        Profile Information
+      </div>
+      <div className="d-flex flex-row main-content-column justify-content-between">
+        <div className="d-flex flex-row justify-content-between main-content-row flex-wrap">
+          <EditProfile attr={this.state.user.attributes} />
+          <EditPassword attr={this.state.user.attributes} />
+          <ShippingInformation attr={this.state.shipping_address} />
+          <PaymentInformation patient={this.state.user.attributes.patient} />
         </div>
       </div>
-  )
+    </div>
+  );
 
-
-  type_to_view = (type) => {
-    switch(type){
+  type_to_view = type => {
+    switch (type) {
       case 'profile_info':
-        return this.patient_info_view()
+        return this.patient_info_view();
       case 'subscription_info':
-        return <SubscriptionInformation user = {this.state.user}/>
+        return <SubscriptionInformation user={this.state.user} />;
       case 'message':
-        return <MessageProcessManager user={this.state.user} view_type="message_box"/>
+        return (
+          <MessageProcessManager
+            user={this.state.user}
+            view_type="message_box"
+          />
+        );
       case 'result':
-        return <Assessment user = {this.state.user}/>
+        return <Assessment user={this.state.user} />;
       default:
-        return "Invalid url"
-    } 
-  }
-  
-  render(){
-    return (
-      this.type_to_view(this.state.type)
-    )
+        return 'Invalid url';
+    }
+  };
+
+  render() {
+    return this.type_to_view(this.state.type);
   }
 }
 
-export default withRouter(connect(null, {get_patient_shipping_address}) (DashboardContents))
-
+export default withRouter(
+  connect(
+    null,
+    { get_patient_shipping_address }
+  )(DashboardContents)
+);
--- src/containers/patient/dashboard.js
+++ src/containers/patient/dashboard.js
@@ -1,110 +1,205 @@
-import React, {Component} from 'react';
-import {withRouter } from "react-router-dom"
-import {connect} from 'react-redux'
-import default_icon from '../../img/user.png'
-import {update_app_state} from '../../actions'
-import {sign_out, update_service_line, update_patient_question_banks} from '../../actions/patient_action'
-import DashboardContents from './dashboard.contents'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import default_icon from '../../img/user.png';
+import { update_app_state } from '../../actions';
+import {
+  sign_out,
+  update_service_line,
+  update_patient_question_banks
+} from '../../actions/patient_action';
+import DashboardContents from './dashboard.contents';
+import './dashboard.scss';
+import cx from 'classnames';
 
-class PatientDashboard extends Component{
-
-  constructor(props){
-    super(props)
+class PatientDashboard extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      view_type:'profile_info', 
-      user:this.props.user,
-      is_ready:false
-    }  
+      view_type: 'profile_info',
+      user: this.props.user,
+      is_ready: false
+    };
   }
 
-  componentDidMount(){
-    console.log("dash board patient Did mount user:", this.props.user)
-    if(!this.props.user.attributes['access-token']){ 
-      this.props.history.push('/patient/sign_in') 
-    }else{
-      this.setState({is_ready:true})
-    }      
-  }
-
-  //updated user information
-  componentWillReceiveProps = (next_props) => { 
-
-    console.log("dash board Will receive patient user:", this.props.user)
-    if(next_props.user.id===null || next_props.user.patient===null){ 
-      this.props.history.push('/patient/sign_in') 
+  componentDidMount() {
+    console.log('dash board patient Did mount user:', this.props.user);
+    if (!this.props.user.attributes['access-token']) {
+      this.props.history.push('/patient/sign_in');
+    } else {
+      this.setState({ is_ready: true });
     }
-    this.setState({user:next_props.user}) 
   }
 
+  // updated user information
+  componentWillReceiveProps = next_props => {
+    console.log('dash board Will receive patient user:', this.props.user);
+    if (next_props.user.id === null || next_props.user.patient === null) {
+      this.props.history.push('/patient/sign_in');
+    }
+    this.setState({ user: next_props.user });
+  };
 
   app_state_checkout_handler = e => {
-		this.props.update_patient_question_banks(['checkout'], 0).then(() => {
-      this.props.history.push('/patient/checkout') 
-    })
-  }
+    this.props.update_patient_question_banks(['checkout'], 0).then(() => {
+      this.props.history.push('/patient/checkout');
+    });
+  };
 
   sign_out_handler = e => {
-    this.props.sign_out() 
-    this.props.history.push('/patient/sign_in') 
-  }
+    this.props.sign_out();
+    this.props.history.push('/patient/sign_in');
+  };
 
-  update_type_handler = (type) => {
-    this.setState({view_type:type}) 
-  }
- 
+  update_type_handler = type => {
+    this.setState({ view_type: type });
+  };
+
   type_to_view = () => {
-    return <DashboardContents user={this.state.user} type={this.state.view_type} />      
-  }
+    return (
+      <DashboardContents user={this.state.user} type={this.state.view_type} />
+    );
+  };
 
+  toggleNavState = () => {
+    this.setState(prevState => ({
+      showNav: !this.state.showNav
+    }));
+  };
 
+  updateType = type => {
+    this.update_type_handler(type);
+    this.toggleNavState();
+  };
 
   view = () => {
-    const type = this.state.view_type
-    const menu_css = "d-flex justify-content-center align-items-center profile-side-item"
-    const last_menu_css = "d-flex justify-content-center align-items-center profile-side-item-last"  
+    const type = this.state.view_type;
+    const last_menu_css = 'd-flex justify-content-center align-items-center profile-side-item-last';
+    const menu = (
+      <>
+        <div
+          className={cx(
+            last_menu_css,
+            type === 'profile_info' ? 'item-selected' : ''
+          )}
+          onClick={() => this.updateType('profile_info')}
+        >
+          Profile Information
+        </div>
+        <div
+          className={cx(
+            last_menu_css,
+            type === 'subscription_info' ? 'item-selected' : ''
+          )}
+          onClick={() => this.updateType('subscription_info')}
+        >
+          Subscription Information
+        </div>
+        <div
+          className={cx(
+            last_menu_css,
+            type === 'message' ? 'item-selected' : ''
+          )}
+          onClick={() => this.updateType('message')}
+        >
+          Messages
+        </div>
+        <div
+          className={cx(
+            last_menu_css,
+            type === 'result' ? 'item-selected' : ''
+          )}
+          onClick={() => this.updateType('result')}
+        >
+          My Assessment Results
+        </div>
+        <div
+          className={cx(last_menu_css, 'mob-only')}
+          onClick={e => this.sign_out_handler()}
+        >
+          Logout
+        </div>
+      </>
+    );
     return (
-      <div className="d-flex flex-row therapist-noprogress">
-        <div className="d-flex flex-column profile-side-bar-holder">
-          <div className="d-flex justify-content-center profile-logo">
-            <a href={process.env.REACT_APP_MAIN_PAGE_URL}><img className="cerebral-logo" src={process.env.PUBLIC_URL + '/img/logo.png'} alt="link to main page"/></a>
-          </div>    
-          <div className="profile-side-items-holder">
-            <div className="profile-side-title-holder">
-              <img className = "profile-image" src={default_icon} alt="link to profile"/> 
-              <div className="d-flex justify-content-center profile-side-title">WELCOME {this.props.user.attributes.first_name}</div> 
-            </div> 
-            <div className= {type==='profile_info'?menu_css+' item-selected':menu_css} onClick={e => this.update_type_handler('profile_info')}>
-              Profile Information
-            </div> 
-            <div className={type==='subscription_info'?menu_css+' item-selected':menu_css} onClick={e => this.update_type_handler('subscription_info')}>
-              Subscription Information
-            </div> 
-            <div className={type==='message'?menu_css+' item-selected':menu_css} onClick={e => this.update_type_handler('message')}>
-              Messages
-            </div> 
-            <div className={type==='result'?last_menu_css+' item-selected':last_menu_css} onClick={e => this.update_type_handler('result')}>
-              My Assessment Results
-            </div> 
-          </div>                
-        </div>
-
-        <div className="d-flex flex-column profile-main-holder">
-          <div className = "profile-main-container">
-            <div className=" d-flex justify-content-end profile-top-menu">
-              <div className = "log-out-holder text-logout" onClick={e=>this.sign_out_handler()}>Logout</div>
+      <div
+        className={cx(
+          'd-flex flex-row therapist-noprogress',
+          this.state.showNav ? 'no-scroll' : ''
+        )}
+      >
+        <div
+          className={cx(
+            'd-flex flex-column profile-side-bar-holder',
+            this.state.showNav ? 'visible' : 'hidden'
+          )}
+        >
+          <div className="header">
+            <div className="d-flex justify-content-center profile-logo">
+              <a href={process.env.REACT_APP_MAIN_PAGE_URL}>
+                <img
+                  className="cerebral-logo"
+                  src={`${process.env.PUBLIC_URL}/img/logo.png`}
+                  alt="link to main page"
+                />
+              </a>
             </div>
-            {this.type_to_view()}            
-         </div>
+            <div
+              className={cx('nav-opener', this.state.showNav ? 'open' : '')}
+              onClick={this.toggleNavState}
+            >
+              <span />
+            </div>
+          </div>
+
+          <div
+            className={cx(
+              'profile-side-items-holder',
+              this.state.showNav ? 'visible' : 'hidden'
+            )}
+          >
+            <div className="profile-side-title-holder desktop-only">
+              <img
+                className="profile-image"
+                src={default_icon}
+                alt="link to profile"
+              />
+              <div className="d-flex justify-content-center profile-side-title">
+                WELCOME {this.props.user.attributes.first_name}
+              </div>
+            </div>
+            {menu}
+          </div>
         </div>
-      </div> 
-    ) 
+        <div className=" d-flex justify-content-end profile-top-menu desktop-only">
+          <div
+            className="log-out-holder text-logout"
+            onClick={e => this.sign_out_handler()}
+          >
+            Logout
+          </div>
+        </div>
+        <div className="d-flex flex-column profile-main-holder">
+          <div className="profile-main-container">{this.type_to_view()}</div>
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    if (this.state.is_ready) return this.view();
+    return null;
   }
-
-
-  render(){ 
-    if(this.state.is_ready) return( this.view() )
-    else return null;
-  } 
 }
 
-export default withRouter(connect(null, {sign_out, update_patient_question_banks, update_app_state, update_service_line}) (PatientDashboard))
+export default withRouter(
+  connect(
+    null,
+    {
+      sign_out,
+      update_patient_question_banks,
+      update_app_state,
+      update_service_line
+    }
+  )(PatientDashboard)
+);
